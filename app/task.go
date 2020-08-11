2cd8b68b49ffcbfa0facfa348dfc752b47dd55ec
zu1k <i@lgf.im>
2020-08-12 06:52:28 +0800

    change license & add more node channel

--- app/task.go
+++ app/task.go
@@ -7,12 +7,18 @@ import (
 
 func CrawlTGChannel() {
 	node := make([]proxy.Proxy, 0)
+
+	// tg上各种节点分享频道
 	node = append(node, getter.NewTGChannelGetter("https://t.me/s/ssrList", 200).Get()...)
 	node = append(node, getter.NewTGChannelGetter("https://t.me/s/SSRSUB", 200).Get()...)
 	node = append(node, getter.NewTGChannelGetter("https://t.me/s/FreeSSRNode", 200).Get()...)
 	node = append(node, getter.NewTGChannelGetter("https://t.me/s/ssrlists", 200).Get()...)
 	node = append(node, getter.NewTGChannelGetter("https://t.me/s/ssrshares", 200).Get()...)
 	node = append(node, getter.NewTGChannelGetter("https://t.me/s/V2List", 200).Get()...)
+	node = append(node, getter.NewTGChannelGetter("https://t.me/s/ssrtool", 200).Get()...)
+	node = append(node, getter.NewTGChannelGetter("https://t.me/s/vmessr", 200).Get()...)
+	node = append(node, getter.NewTGChannelGetter("https://t.me/s/FreeSSR666", 200).Get()...)
+	node = append(node, getter.NewTGChannelGetter("https://t.me/s/fanqiang666", 200).Get()...)
 
 	node = append(node, GetProxies()...)
 	node = proxy.Deduplication(node)
