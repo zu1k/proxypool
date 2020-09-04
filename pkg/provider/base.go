1b1a8014f34976e4da6f30b7c5e024bd21129fc9
zu1k <i@lgf.im>
2020-09-04 16:40:31 +0800

    add sub support

--- pkg/provider/base.go
+++ pkg/provider/base.go
@@ -1,5 +1,11 @@
 package provider
 
+import (
+	"strings"
+
+	"github.com/zu1k/proxypool/pkg/proxy"
+)
+
 type Provider interface {
 	Provide() string
 }
@@ -11,4 +17,73 @@ func checkInList(list []string, item string) bool {
 		}
 	}
 	return false
+}
+
+type Base struct {
+	Proxies    *proxy.ProxyList `yaml:"proxies"`
+	Types      string           `yaml:"type"`
+	Country    string           `yaml:"country"`
+	NotCountry string           `yaml:"not_country"`
+}
+
+func (b Base) preFilter() {
+	proxies := make(proxy.ProxyList, 0)
+
+	needFilterType := true
+	needFilterCountry := true
+	needFilterNotCountry := true
+	if b.Types == "" || b.Types == "all" {
+		needFilterType = false
+	}
+	if b.Country == "" || b.Country == "all" {
+		needFilterCountry = false
+	}
+	if b.NotCountry == "" {
+		needFilterNotCountry = false
+	}
+	types := strings.Split(b.Types, ",")
+	countries := strings.Split(b.Country, ",")
+	notCountries := strings.Split(b.NotCountry, ",")
+
+	bProxies := *b.Proxies
+	for _, p := range bProxies {
+		if needFilterType {
+			typeOk := false
+			for _, t := range types {
+				if p.TypeName() == t {
+					typeOk = true
+					break
+				}
+			}
+			if !typeOk {
+				goto exclude
+			}
+		}
+
+		if needFilterNotCountry {
+			for _, c := range notCountries {
+				if strings.Contains(p.BaseInfo().Name, c) {
+					goto exclude
+				}
+			}
+		}
+
+		if needFilterCountry {
+			countryOk := false
+			for _, c := range countries {
+				if strings.Contains(p.BaseInfo().Name, c) {
+					countryOk = true
+					break
+				}
+			}
+			if !countryOk {
+				goto exclude
+			}
+		}
+
+		proxies = append(proxies, p)
+	exclude:
+	}
+
+	b.Proxies = &proxies
 }
