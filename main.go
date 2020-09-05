5b5999834d1a07006a5c7f03508bcc31060a12a9
zu1k <i@lgf.im>
2020-09-05 21:08:58 +0800

    add debug pprof

--- main.go
+++ main.go
@@ -3,19 +3,24 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
-
-	"github.com/zu1k/proxypool/internal/database"
 
 	"github.com/zu1k/proxypool/api"
 	"github.com/zu1k/proxypool/internal/app"
 	"github.com/zu1k/proxypool/internal/cron"
+	"github.com/zu1k/proxypool/internal/database"
 	"github.com/zu1k/proxypool/pkg/proxy"
 )
 
 var configFilePath = ""
 
 func main() {
+	go func() {
+		http.ListenAndServe("0.0.0.0:6060", nil)
+	}()
+
 	flag.StringVar(&configFilePath, "c", "", "path to config file: config.yaml")
 	flag.Parse()
 
